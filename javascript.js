5792bfe9e670ad36ba55983318f6657547bf66f2
Sam Cheeseman <182329632+LeCheeseman@users.noreply.github.com>
2024-10-10 13:05:42 +0100

    Final version. Fully working.

--- javascript.js
+++ javascript.js
@@ -6,8 +6,10 @@ let compScore = 0
 
 function getComputerChoice() {
     
+// We use a random number between 1-9 because there are three results
+// so we can distribute them evenly 1-3, 4-6, 7-9.
+
     let compRandomNumber = Math.floor(Math.random() * (max - min)) + 1;
- //   console.log(compRandomNumber + " is the random number");
    
     if (compRandomNumber <= 3) { 
         return "rock";
@@ -22,11 +24,7 @@ function getComputerChoice() {
 
 }
 
-// We use a random number between 1-9 because there are three results
-// so we can distribute them evenly. 
-
 function getHumanChoice() {
-    getComputerChoice();
     let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
     return humanChoice;
     console.log(humanChoice);
@@ -42,58 +40,47 @@ function playRound() {
         console.log("Computer: Paper!");
         console.log("You lose! Paper beats rock.");
         ++compScore
-//        checkScore();
     }
         else if (humanChoice == "rock" && compChoice == "scissors") {
             console.log("You: Rock...");
             console.log("Computer: Scissors!");
             console.log("You win! Rock beats scissors.");
             ++humanScore
- //           checkScore();
-
         }
          else if (humanChoice == "rock" && compChoice == "rock") {
             console.log("You: Rock...");
             console.log("Computer: Rock!");
             console.log("It's a draw! Rock and rock.");
          }
-   
     if (humanChoice == "paper" && compChoice == "rock") {
             console.log("You: Paper...");
             console.log("Computer: Rock!");
             console.log("You win! Paper beats rock.");
             ++humanScore
-//            checkScore();
-
         }
         else if (humanChoice == "paper" && compChoice == "scissors") {
                 console.log("You: Paper...");
                 console.log("Computer: Scissors!");
                 console.log("You lose! Scissors beats paper.");
                 ++compScore
-//               checkScore();
-
             }
         else if (humanChoice == "paper" && compChoice == "paper") {
             console.log("You: Paper...");
             console.log("Computer: Paper!");
             console.log("It's a draw! Paper and paper.");
             }
-        
     if (humanChoice == "scissors" && compChoice == "rock") {
             console.log("You: Scissors...");
             console.log("Computer: Rock!");
             console.log("You lose! Rock beats scissors.");
-                ++compScore
-//                checkScore();
+            ++compScore
 
             }
         else if (humanChoice == "scissors" && compChoice == "paper") {
             console.log("You: Scissors...");
             console.log("Computer: Paper!");
             console.log("You win! Scissors beats paper.");
-                ++humanScore
-//                    checkScore();
+            ++humanScore
 
                 }
         else if (humanChoice == "scissors" && compChoice == "scissors") {
@@ -111,9 +98,6 @@ function playGame() {
             console.log("Game Over!")
             finalScore();
         }
-        
-    
-    
 
  function finalScore() {
     console.log('');
@@ -123,32 +107,14 @@ function playGame() {
     if (humanScore > compScore) console.log("You WIN!");
     if (compScore > humanScore) console.log("You LOSE!");
     if (humanScore === compScore) console.log("It's a draw!");
-
+    resetScore();
  }
 
-// function checkScore() {
-
-   // if (humanScore === 5) {
-     //   console.log(" You are the winner! ");
-//        console.log(" Final Score: ")
-//        console.log("Player: " + humanScore)
-//        console.log("Computer: " + compScore)
-//        let humanScore = 0
-//        let compScore = 0
-//    }
-
-//    else if (compScore === 5) {
-//            console.log(" Game Over! You lose! ");
-//            console.log(" Final Score: ")
-//            console.log("Computer: " + compScore)
-//            console.log("Player: " + humanScore)
-//            let humanScore = 0
-//            let compScore = 0
-//
- //   }
-//    else {
-//        playRound();
-//    }
+ function resetScore() {
+    // We're resetting global variables, so don't need 'let' 
+    humanScore = 0;
+    compScore = 0;
+ }
 
 
 
